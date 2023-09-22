de17fdd36c2c9c825354427afcfcf032bde5ce48
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-22 08:42:54 -0500

    [ADD]: custom metrics with micrometer + prometheus

--- 02_configserver-client/dragonball/src/main/java/com/alex/dragonball/controllers/ApplicationNameController.java
+++ 02_configserver-client/dragonball/src/main/java/com/alex/dragonball/controllers/ApplicationNameController.java
@@ -1,6 +1,7 @@
 package com.alex.dragonball.controllers;
 
 import com.alex.dragonball.config.DragonBallConfig;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 
 @RestController
@@ -17,12 +20,26 @@ public class ApplicationNameController {
     @Autowired
     private DragonBallConfig configuration;
 
+    // // Metrics  <--  X las deps, Spring ya sabe en donde esta este Bean, asi q simplemente lo Inyectamos
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     private static final Logger log = LoggerFactory.getLogger(ApplicationNameController.class);
 
 
     @GetMapping
     public ResponseEntity<String> getAppName() {
         log.info("Getting Application Name");
+
+
+        // // custom metrics: metric name, tags in pairs key-value
+        int value = new Random().nextInt(20);
+
+        // condicionar el Value del TAG de esta metric  | consultar x tag: actuator/metrics/alx.dragonball.name?tag=level:jr
+        meterRegistry.counter("alx.dragonball.name", "level", (value < 3) ? "jr" : "sr")
+                .increment(value);  // incrementamos el counter con 1 random
+
+
         return ResponseEntity.ok(configuration.getApplicationName());
     }
 
