626ae936d95a039423915127175bcbeb19178155
Adrian Changalombo <alextravc.342@gmail.com>
2023-09-23 20:38:37 -0500

    [ADD]: supplier + function + consumer

--- 06_cloud-stream/cloudstream-example/src/main/java/com/alx/cloudstreamexample/CloudstreamExampleApplication.java
+++ 06_cloud-stream/cloudstream-example/src/main/java/com/alx/cloudstreamexample/CloudstreamExampleApplication.java
@@ -5,8 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class CloudstreamExampleApplication {
@@ -15,6 +19,7 @@ public class CloudstreamExampleApplication {
 
 
     // // Ya se conecta en auto y en este caso transforma la informacion q emite el producer
+    // Crea los   TOPICS    con el   NOMBRE de la Fn
     @Bean
     public Function<String, String> toUpperCase() {
         return data -> {
@@ -23,6 +28,27 @@ public class CloudstreamExampleApplication {
             return data.toUpperCase();
         };
         //return String::toUpperCase;
+    }
+
+
+    // // // Supplier / Producer   >>   Processor | Function   >>   Consumer      <--   para q funcionen con  CloudStream  deben ser @Bean
+    // // producer-out-0
+    @Bean
+    public Supplier<Flux<Long>> producer() {
+        return () -> Flux.interval(Duration.ofSeconds(1)).log();
+    }
+
+    // // processor-in-0  |  processor-out-0
+    @Bean
+    public Function<Flux<Long>, Flux<Long>> processor() {
+        return flx -> flx.map(n -> n * n);  // return el cuadrado del num q recibe
+    }
+
+    @Bean
+    public Consumer<Long> consumer() {  // ya recibe simplemente el Long
+        return (number) -> {
+            log.info("Message received: '{}'", number);
+        };
     }
 
 
